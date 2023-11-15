41beba76bb4f5d409031f3e009097627c8696fd9
Betulioo <luis.nunez@bootcamp-upgrade.com>
2023-11-15 10:13:14 +0100

    pokemons list and base template

--- src/Controller/PokemonsController.php
+++ src/Controller/PokemonsController.php
@@ -18,4 +18,39 @@ class PokemonsController extends AbstractController {
         ];
             return $this -> render("Pokemons/showPokemon.html.twig", ["pokemon" => $pokemon]);
     }
+
+
+    #[Route("/pokemons")]
+public function listPokemons(){
+        $pokemons = [
+
+            [
+                "name" => "Bulbasaur",
+                "description" => "Bulbasaur is the first Pokémon species discovered in the wild. It",
+                "image" => "https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png",
+                "code" => "001"
+            ],
+            [
+                "name" => "charmander",
+                "description" => "charmander is the first Pokémon species discovered in the wild. It",
+                "image" => "https://assets.pokemon.com/assets/cms2/img/pokedex/full/002.png",
+                "code" => "002"
+            ],
+            [
+                "name" => "squirtle",
+                "description" => "squirtle is the first Pokémon species discovered in the wild. It",
+                "image" => "https://assets.pokemon.com/assets/cms2/img/pokedex/full/003.png",
+                "code" => "001"
+            ],
+            [
+                "name" => "pikachu",
+                "description" => "pikachu is the first Pokémon species discovered in the wild. It",
+                "image" => "https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png",
+                "code" => "001"
+            ]
+
+        ];
+        return $this -> render("Pokemons/listPokemons.html.twig", ["pokemons" => $pokemons]);
+
+    }
 }
